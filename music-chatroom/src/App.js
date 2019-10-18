c56ea0ff49d7ccef6f1ffdb246493c26f9de8e4b
nick_klabz <nklabz52@gmail.com>
2019-10-18 15:22:38 -0500

    initial commit

---
+++ music-chatroom/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="HomePage">
+      <header className="Home-Page">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
