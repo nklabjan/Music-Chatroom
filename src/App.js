2e8a9e276febfd1ad024b4801befe978d61e32b6
nklabjan <nklabz52@gmail.com>
2019-10-20 16:04:17 -0500

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="HomePage">
+      <header className="Home-Page">
+        <button>Login with Spotify</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
