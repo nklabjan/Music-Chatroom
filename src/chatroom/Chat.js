6f84d400c4f90d428c14de06294f2811a883652b
James Kuoppala <kuoppala@wisc.edu>
2019-10-22 17:51:56 -0500

    added chat page and chat css file

---
+++ src/chatroom/Chat.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import '../css/Chat.css';
+
+class Chat extends Component {
+
+    render() {
+        return <div>
+            <button>This is a button</button>
+        </div>
+    }
+}
+
+export default Chat;
