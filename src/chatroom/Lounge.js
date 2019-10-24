866966a86d098b9c2f3d43d27ff000e71255847e
nklabjan <nklabz52@gmail.com>
2019-10-24 11:12:59 -0500

    lounge buttons

--- src/chatroom/Lounge.js
+++ src/chatroom/Lounge.js
@@ -1,15 +1,25 @@
 import React, {Component} from "react";
+import ReactDOM from 'react-dom';
 import '../css/Lounge.css';
 import ChatDisplay from './ChatDisplay';
 import ChatBar from './ChatBar';
 import Queue from './Queue';
 import UserList from './UserList';
+import App from '../homepage/App';
 
 class Lounge extends Component {
+
+    leaveChat() {
+
+    }
 
     render() {
         return (
             <>
+                <header class="chatroom-header">
+                    <button class="profile-chatroom">View/Edit Your Profile</button>
+                    <button class="leave-chatroom" onClick={this.leaveChat.bind(this)}>Leave Chatroom</button>
+                </header>
                 <div class="container">
                     <Queue />
                     <ChatDisplay />
