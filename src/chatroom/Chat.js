a5f49f46656c8df1638636afa043b52a1abb11b3
nklabjan <nklabz52@gmail.com>
2019-10-29 16:57:49 -0500

    text message

--- src/chatroom/Chat.js
+++ src/chatroom/Chat.js
@@ -13,6 +13,8 @@ class Chat extends Component {
                 <div className="chatDisplay">
                     <div className="text">
                         <div className="inputMessage">
+                            <textarea className="textarea">
+                            </textarea>
                         </div>
                         <button className="sendMessage" onClick={this.sendMessage.bind(this)}>Send</button>
                     </div>
