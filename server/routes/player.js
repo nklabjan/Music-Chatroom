2363c5efbab5d0f45a9d3f7d1f6d96ffe08ace52
nevillenzf <nzng@wisc.edu>
2019-10-29 17:07:06 -0500

    Added player route to backend

--- server/routes/player.js
+++ server/routes/player.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET player base page. */
 router.get('/', function(req, res, next) {
   console.log(next);
   res.send('respond with a player resource');
+});
+
+/* GET users listing. */
+router.get('/get', function(req, res) {
+  res.send('send base player info to client');
 });
 
 module.exports = router;
