6ed4fdbec73e825eb6bfff5c19953e5d3d5f71d8
nevillenzf <nzng@wisc.edu>
2019-10-31 16:11:52 -0500

    Added Spotify logo

--- src/Components/homepage/LandingWindow.js
+++ src/Components/homepage/LandingWindow.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import {Button} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpotify } from '@fortawesome/free-brands-svg-icons'
 
 class LandingWindow extends Component {
   //{*/ <button className="login" onClick={this.props.login}>Login with Spotify</button> */}
@@ -10,7 +12,7 @@ class LandingWindow extends Component {
                     <div className="LandingBtnBox">
                     <Button className="new-login"
                             size="lg"
-                            onClick={this.props.login}>Login with Spotify </Button>
+                            onClick={this.props.login}> <FontAwesomeIcon icon={faSpotify} /> Login with Spotify </Button>
                     </div>
                 </div>
       }
