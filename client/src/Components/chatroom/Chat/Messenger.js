091eb1e21828ad1c2826f2b6cd56b35ed5a62eba
Tomas Larrain <39778986+tlarrain98@users.noreply.github.com>
2019-11-01 16:49:45 -0500

    enter sends chat now

--- client/src/Components/chatroom/Chat/Messenger.js
+++ client/src/Components/chatroom/Chat/Messenger.js
@@ -3,10 +3,17 @@ import '../../../css/chatroom/chat/Messenger.css';
 
 class Messenger extends Component {
 
+    onEnterPress = (e) => {
+        if(e.keyCode == 13 && e.shiftKey == false) {
+            e.preventDefault();
+            this.props.sendMessage();
+        }
+    }
+    
     render() {
         return (
           <div className="messenger">
-              <textarea className="textarea" id="textarea"></textarea>
+              <textarea className="textarea" id="textarea" onKeyDown={this.onEnterPress}></textarea>
               <button className="sendMessage" onClick={this.props.sendMessage}>Send</button>
           </div>
         )
