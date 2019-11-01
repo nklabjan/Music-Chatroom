9313c388dd5613f4d06c67e423d958ede460fd34
bingowas <michaelbryanroy@gmail.com>
2019-11-01 17:33:28 -0500

    Updates to link so it works correctly both on dev server and on heroku

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ContentHandler from './Components/ContentHandler';
 
-class App extends Component {
-
+class App extends Component { 
   render() {
       return (
         <>
