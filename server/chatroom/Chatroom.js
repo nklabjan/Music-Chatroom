7b43b50ad47bca4ba73f486ea45727672380b9f1
Michael <michaelbryanroy@gmail.com>
2019-11-02 22:11:59 -0500

    Added chat history and maintaining a list of users

--- server/chatroom/Chatroom.js
+++ server/chatroom/Chatroom.js
@@ -1,19 +1,39 @@
-const members = new Map();
+class Chatroom {
+    constructor(io) {
+        this.userCount = 0;
+        this.request = require('request');
+        this.io = io;
+        // cache of sockets against user info
+        this.users = {};
+        this.messageList = [];
+    }
+    userConnected(socket, accessToken) {
+        // when a new user connects, get profile information
+        console.log(this.messageList);
+        var url = 'https://api.spotify.com/v1/me?access_token=' +accessToken;
+        var chatroom = this; // alias this as chatroom so it can be referenced in async call
+        this.request(url, function(error, response, body) {
+            body = JSON.parse(body);
+            chatroom.users[socket] = body["display_name"];
+        })
 
-let chatHistory = [];
+        for (var indx in this.messageList) {
+            var msgPayload = this.messageList[indx];
+            //this.io.emit("message_received", msgPayload['msg'], msgPayload['user']);
+            socket.emit("message_received", msgPayload['msg'], msgPayload['user']);
+        }
 
-function broadcastMessage(message) {
-    members.forEach(m => m.emit('message_sent', message));
+    }
+
+    playSong(accessToken, deviceId, spotifyURI)
+    {
+        
+    }
+
+    chatMessage(socket, message) {
+        this.messageList.push({'user': this.users[socket], 'msg' : message });
+        this.io.emit("message_received", message, this.users[socket]);
+    }
 }
 
-function addEntry(entry) {
-    chatHistory = chatHistory.concat(entry);
-}
-
-function getChatHistory() {
-    return chatHistory.slice();
-}
-
-function addUser() {
-    
-}
+module.exports.Chatroom = Chatroom;
