7c59594b666d42cda73e19a34ee1b669dc61ab6a
Michael <michaelbryanroy@gmail.com>
2019-11-03 15:15:55 -0600

    Using socket.id as userList id on server

--- server/chatroom/Chatroom.js
+++ server/chatroom/Chatroom.js
@@ -8,6 +8,7 @@ class Chatroom {
     }
     userConnected(socket, accessToken) {
         // when a new user connects, get profile information
+        console.log(socket.id);
         var url = 'https://api.spotify.com/v1/me?access_token=' +accessToken;
         var chatroom = this; // alias this as chatroom so it can be referenced in async call
         var io = this.io;
@@ -16,8 +17,8 @@ class Chatroom {
             console.log(chatroom.users[socket] !== undefined);
             // if this is a socket we haven't seen before, add it to the user list
             // and emit a join message to everyone else
-            if (chatroom.users[socket] === undefined) {
-                chatroom.users[socket] = body["display_name"];
+            if (chatroom.users[socket.id] === undefined) {
+                chatroom.users[socket.id] = body["display_name"];
                 socket.broadcast.emit("user_connected", body["display_name"]);
             }
             // regardless, load the member list
@@ -44,8 +45,8 @@ class Chatroom {
     }
 
     chatMessage(socket, message) {
-        this.messageList.push({'user': this.users[socket], 'msg' : message });
-        this.io.emit("message_received", message, this.users[socket]);
+        this.messageList.push({'user': this.users[socket.id], 'msg' : message });
+        this.io.emit("message_received", message, this.users[socket.id]);
     }
 }
 
