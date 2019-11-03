c9c47f803a37e0a475e6a46e83f9708e726faa6b
nevillenzf <nzng@wisc.edu>
2019-11-03 17:03:38 -0600

    Added .env in client directory to gitignore

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-const frontend_uri = (process.env.REACT_APP_FRONTEND_URI || 'http://localhost:3000');
-const backend_url = (process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080');
+var dotenv = require('dotenv').config();
+const frontend_uri = (process.env.FRONTEND_URI || 'http://localhost:3000');
+const backend_url = (process.env.BACKEND_URL || 'http://localhost:8080');
 
 module.exports.frontend_uri = frontend_uri;
 module.exports.backend_url = backend_url;
-
