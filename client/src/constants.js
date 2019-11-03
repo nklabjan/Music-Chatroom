63b0c3591fec27f41e8da687b528b3d81a817938
Michael <michaelbryanroy@gmail.com>
2019-11-03 11:45:17 -0600

    Added REACT_APP prefix to client environment variables

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
-const frontend_uri = (process.env.FRONTEND_URI || 'http://localhost:3000');
-const backend_url = (process.env.BACKEND_URL || 'http://localhost:8080');
+const frontend_uri = (process.env.REACT_APP_FRONTEND_URI || 'http://localhost:3000');
+const backend_url = (process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080');
 
 module.exports.frontend_uri = frontend_uri;
 module.exports.backend_url = backend_url;
