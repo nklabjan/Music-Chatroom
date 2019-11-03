7654273a49885a32a31ae66e222b42315ffea6d6
Michael <michaelbryanroy@gmail.com>
2019-11-02 22:55:28 -0500

    Added user list functionality

--- server/chatroom/Chatroom.js
+++ server/chatroom/Chatroom.js
@@ -1,6 +1,5 @@
 class Chatroom {
     constructor(io) {
-        this.userCount = 0;
         this.request = require('request');
         this.io = io;
         // cache of sockets against user info
@@ -9,12 +8,24 @@ class Chatroom {
     }
     userConnected(socket, accessToken) {
         // when a new user connects, get profile information
-        console.log(this.messageList);
         var url = 'https://api.spotify.com/v1/me?access_token=' +accessToken;
         var chatroom = this; // alias this as chatroom so it can be referenced in async call
+        var io = this.io;
         this.request(url, function(error, response, body) {
             body = JSON.parse(body);
-            chatroom.users[socket] = body["display_name"];
+            console.log(chatroom.users[socket] !== undefined);
+            // if this is a socket we haven't seen before, add it to the user list
+            // and emit a join message to everyone else
+            if (chatroom.users[socket] === undefined) {
+                chatroom.users[socket] = body["display_name"];
+                socket.broadcast.emit("user_connected", body["display_name"]);
+            }
+            // regardless, load the member list
+            for(var userId in chatroom.users) {
+                socket.emit("user_connected", chatroom.users[userId]);
+            }
+
+
         })
 
         for (var indx in this.messageList) {
@@ -23,7 +34,9 @@ class Chatroom {
             socket.emit("message_received", msgPayload['msg'], msgPayload['user']);
         }
 
+
     }
+
 
     playSong(accessToken, deviceId, spotifyURI)
     {
