04aac5ca2afa192a11f888500457445fe88652dc
nklabjan <nklabz52@gmail.com>
2019-11-05 14:41:50 -0600

    hi

---
+++ src/Components/makechatroom/MakeChatDetails.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import '../../css/makechatroom/MakeChatDetails.css';
+
+class MakeChatDetails extends Component {
+
+    render() {
+        return (
+            <>
+                <div className="makeChatDet">
+                    
+                </div>
+            </>
+        );
+    }
+}
+
+export default MakeChatDetails;
