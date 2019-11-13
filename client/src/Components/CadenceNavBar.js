8c4161362e8ace2cb5ae1cd14322d6a14b7516d1
nklabjan <nklabz52@gmail.com>
2019-11-13 15:40:27 -0600

    navbar

--- client/src/Components/CadenceNavBar.js
+++ client/src/Components/CadenceNavBar.js
@@ -2,10 +2,10 @@ import React, {Component} from "react";
 import {Navbar, Nav, Button} from 'react-bootstrap';
 
 class CadenceNavBar extends Component {
+
     //Include selective rendering for content
     selectivelyRender(){
-      if (this.props.scheme === "CadenceNavBarInit")
-      {
+      if (this.props.scheme === "CadenceNavBarInit") {
         return (
           <Navbar variant="dark"
                   className={this.props.scheme}
@@ -24,21 +24,18 @@ class CadenceNavBar extends Component {
                           fixed={this.props.scheme === "CadenceNavBarInit" ? "top" : ""}>
                   <Navbar.Brand >Cadence</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link onClick={this.props.handleHome}>Home</Nav.Link>
+          {this.props.currDisplay === "lounge" ? <Nav.Link onClick={this.props.handleHome}>Home</Nav.Link> : <></>}
           <Nav.Link onClick={this.props.handleProfile}>Profile</Nav.Link>
-          <Nav.Link onClick={this.props.handleMakeChat}>Make Lounge</Nav.Link>
-          <Nav.Link onClick={this.props.currDisplay === "lounge" ?
-                              this.props.handleHome : this.props.handleChat}>
-            {this.props.currDisplay === "lounge" ? "Leave Lounge": "Go To Lounge"}
-          </Nav.Link>
+          {this.props.currDisplay === "home" ? <Nav.Link onClick={this.props.handleMakeChat}>Make Lounge</Nav.Link> : <></>}
+          {this.props.currDisplay === "lounge" ? <Nav.Link onClick={this.props.handleHome}>Leave Lounge</Nav.Link> : <></>}
         </Nav>
         <Nav id="outline-info">
           <Nav.Link >
-            <Button onClick={this.props.logout}> Logout </Button>
+            <Button onClick={this.props.logout}> Logout</Button>
           </Nav.Link>
         </Nav>
         </Navbar>
-)
+        )
       }
     }
 
