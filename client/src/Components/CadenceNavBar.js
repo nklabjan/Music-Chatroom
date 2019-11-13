a47d270b009fd7702cc1f1eb1afb1d30ed5f030d
nevillenzf <nzng@wisc.edu>
2019-11-12 18:14:49 -0600

    Changed NavBar to persist throughout the website

--- client/src/Components/CadenceNavBar.js
+++ client/src/Components/CadenceNavBar.js
@@ -3,19 +3,47 @@ import {Navbar, Nav} from 'react-bootstrap';
 
 class CadenceNavBar extends Component {
     //Include selective rendering for content
+    selectivelyRender(){
+      if (this.props.scheme === "CadenceNavBarInit")
+      {
+        return (
+          <Navbar variant="dark"
+                  className={this.props.scheme}
+                  fixed={this.props.scheme === "CadenceNavBarInit" ? "top" : ""}>
+
+            <Navbar.Brand >Cadence</Navbar.Brand>
+            <Nav className="mr-auto">
+          <Nav.Link href="https://www.spotify.com/premium/">Get Spotify Premium</Nav.Link>
+          <Nav.Link>Who Are We?</Nav.Link>
+        </Nav>
+        </Navbar>)
+      }
+      else {
+        return ( <Navbar  variant="dark"
+                          className={this.props.scheme}
+                          fixed={this.props.scheme === "CadenceNavBarInit" ? "top" : ""}>
+                  <Navbar.Brand >Cadence</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link onClick={this.props.handleHome}>Home</Nav.Link>
+          <Nav.Link onClick={this.props.handleProfile}>Profile</Nav.Link>
+          <Nav.Link onClick={this.props.handleMakeChat}>Make Lounge</Nav.Link>
+          <Nav.Link onClick={this.props.currDisplay === "lounge" ?
+                              this.props.handleHome : this.props.handleChat}>
+            {this.props.currDisplay === "lounge" ? "Leave Lounge": "Go To Lounge"}
+          </Nav.Link>
+        </Nav>
+        <Nav id="outline-info">
+          <Nav.Link onClick={this.props.logout}>Logout</Nav.Link>
+        </Nav>
+        </Navbar>
+)
+      }
+    }
 
     render() {
-        return  <div >
-        <Navbar variant="dark"  className={this.props.scheme}
-                                fixed={this.props.scheme === "CadenceNavBarInit" ? "top" : ""}>
-          <Navbar.Brand >Cadence</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link href="https://www.spotify.com/premium/">Get Spotify Premium</Nav.Link>
-            <Nav.Link>Who Are We?</Nav.Link>
-          </Nav>
-        </Navbar>
-                </div>
+        return  (<div >
+          {this.selectivelyRender()}
+          </div>)
       }
 
 }
