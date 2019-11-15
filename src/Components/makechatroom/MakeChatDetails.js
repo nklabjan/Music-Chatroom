aee9a0f8e32d873057bf3322eb31b779169242c0
nklabjan <nklabz52@gmail.com>
2019-11-15 10:46:17 -0600

    fdasfs

--- src/Components/makechatroom/MakeChatDetails.js
+++ src/Components/makechatroom/MakeChatDetails.js
@@ -30,8 +30,11 @@ class MakeChatDetails extends Component {
                                 <option>R&B/Soul</option>
                                 <option>Rock</option>
                                 </Form.Control>) :
-                            ( this.props.label === "formDescription" ? <textarea className="makeChatInput" maxlength="100" id={this.props.label} cols="30" rows="4"></textarea> : 
-                            <textarea className="makeChatInput" maxlength="20" id={this.props.label} cols="30" rows="4"></textarea> )}
+                            ( this.props.label === "formDescription" ? <textarea className="makeChatInput" maxlength="100" id={this.props.label} cols="60" rows="2"></textarea> : 
+                            <textarea className="makeChatInput" maxlength="20" id={this.props.label} cols="60" rows="2"></textarea> )}
+                        {this.props.label === "formGenres" ? <></> :
+                            this.props.label === "formDescription" ? <div className="restriction">max characters = 100</div> :
+                            <div className="restriction">max characters = 20</div>}
                     </div>
                 </div>
             </>
