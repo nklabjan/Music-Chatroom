6854f290e4c6d5d3c301f3a8a780ac825338fe31
Tomas Larrain <39778986+tlarrain98@users.noreply.github.com>
2019-11-14 22:27:31 -0600

    qweweg

--- src/Components/chatroom/Queue/QueueDeck.js
+++ src/Components/chatroom/Queue/QueueDeck.js
@@ -1,37 +1,34 @@
 import React, {Component} from "react";
 import { Table } from 'react-bootstrap';
-import QueueCard from './QueueCard';
-import '../../../css/chatroom/Queue.css'
+import Song from './Song';
+import '../../../css/chatroom/Queue.css';
+import SongList from './SongList';
+
+let songList = new SongList();
 
 class QueueDeck extends Component {
+    constructor() {
+      super();
+    }
 
+    getSongs() {
+      let songs = songList.getList();
+      let queue = [];
+
+      for(var i = 0; i < songs.length; i++) {
+        queue.push(
+          <Song uri={songs[i]} access_token={this.props.access_token}/>
+        )
+      }
+      if(Object.entries(queue).length !== 0) {
+        return queue;
+      }
+    }
     render() {
-        return (
-                <div className="QueueDeck">
-                    <div className="QueueDeckTitle">Queue</div>
-                      <Table striped bordered hover variant="dark" className="queueTable">
-                        <thead>
-                          <tr>
-                            <th></th>
-                            <th>Title</th>
-                            <th>Artist</th>
-                            <th>Album</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            <QueueCard  title={"All Star"}
-                                        artist={"Smash Mouth"}
-                                        album={"Astro Lounge"}
-                                        uri={"spotify:track:3cfOd4CMv2snFaKAnMdnvK"}
-                                        socket={this.socket}/>
-                            <QueueCard  title={"Stacy's Mom"}
-                                        artist={"Fountain of Wayne"}
-                                        album={"Welcome interstate Manager"}
-                                        uri={"spotify:track:27L8sESb3KR79asDUBu8nW"}
-                                        socket={this.socket}/>
-                        </tbody>
-                      </Table>
-                </div>
+        return(
+          <div>
+            {this.getSongs()}
+          </div>
         )
     }
 }
