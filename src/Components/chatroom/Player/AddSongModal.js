d23db5cc8b6d53ac24139ad3749ccca1f443b313
Tomas Larrain <39778986+tlarrain98@users.noreply.github.com>
2019-11-21 17:11:04 -0600

    asdfasdf2

---
+++ src/Components/chatroom/Player/AddSongModal.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+class AddSongModal extends Component {
+    render() {
+        return(
+            <Modal>
+                <Modal.Header>Add Song</Modal.Header>
+            </Modal>
+        )
+    }
+}
+
+export default AddSongModal;
