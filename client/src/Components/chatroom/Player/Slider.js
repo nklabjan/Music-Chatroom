a3f4add299a05709af54bbc1f0482872144fee51
nevillenzf <nzng@wisc.edu>
2019-11-21 20:54:46 -0600

    nothing

--- client/src/Components/chatroom/Player/Slider.js
+++ client/src/Components/chatroom/Player/Slider.js
@@ -5,19 +5,26 @@ import '../../../css/chatroom/player/Slider.css'
 
 class SliderCom extends Component {
 
-    componentWillMount(){
-      //At mount set seek to currently played position
-
+    millisToMinutesAndSeconds(millis) {
+      var minutes = Math.floor(millis / 60000);
+      var seconds = ((millis % 60000) / 1000).toFixed(0);
+      return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
+    
+    componentDidMount(){
+      console.log(this.props.duration);
     }
 
     render() {
         return (
               <div className="sliderDiv">
-                <div className="position">{this.props.position}</div>
+                <div className="position">{this.millisToMinutesAndSeconds(this.props.position)}</div>
                 <div className="sliderContainer">
-                  <Slider className="mySlider" onAfterChange={this.props.handleSeeking}/>
+                  <Slider className="mySlider" onAfterChange={this.props.handleSeeking}
+                          defaultValue={this.props.position/this.props.duration * 100}
+                          value={(value)=>{}}/>
                 </div>
-                <div className="duration">{this.props.duration}</div>
+                <div className="duration">{this.millisToMinutesAndSeconds(this.props.duration)}</div>
               </div>
         )
     }
