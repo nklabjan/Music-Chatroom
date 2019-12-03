a81928d47ded1c0f91d32c4c9c6e38b15a1796e9
Tomas Larrain <39778986+tlarrain98@users.noreply.github.com>
2019-12-02 21:38:14 -0600

    asdfasdfasdgweg

--- client/src/Components/chatroom/Player/AddSongModal.js
+++ client/src/Components/chatroom/Player/AddSongModal.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import Song from '../Queue/Song';
 
 class AddSongModal extends Component {
     constructor(props) {
@@ -13,8 +14,9 @@ class AddSongModal extends Component {
     }
 
     async getSongs() {
-        let value = this.search.current.value.split(' ').join('+');
-        const response = await fetch('https://api.spotify.com/v1/search/?q=' + value + '&type=track', {
+        let value = this.search.current.value.split(' ').join('+'); 
+        let url = 'https://api.spotify.com/v1/search/?q=' + value + '&type=track&limit=5'; 
+        const response = await fetch(url, {
             method: "GET",
             headers: {
                 authorization: `Bearer ${this.props.access_token}`,
@@ -22,7 +24,24 @@ class AddSongModal extends Component {
         });
         const myJson = await response.json();
         this.setState({ data: myJson });
-        console.log(this.state.data);
+        //console.log(this.state.data);
+    }
+
+    displaySongs() {
+        console.log(this.search);
+        if(this.props.show === true) {
+            let songs = [];
+
+            if (Object.entries(this.state.data).length !== 0) {
+                for (const song in Object.entries(this.state.data.tracks.items)) {
+                    songs.push(
+                        <Song data={this.state.data.tracks.items[song]} />
+                    )
+                }
+                return songs
+            }
+            return "Songs will be displayed as you search for them."
+        }
     }
 
     render() {
@@ -36,6 +55,7 @@ class AddSongModal extends Component {
                             <Form.Control type='text' placeholder='Search' autoComplete='off' ref={this.search} />
                         </Form.Group>
                     </Form>
+                    {this.displaySongs()}
                 </Modal.Body>
             </Modal>
         )
