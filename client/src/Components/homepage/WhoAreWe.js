7c981583850e2b79af327cd90d30195271e7c8fc
Brian Portland <bponline@comcast.net>
2019-12-03 19:18:33 -0600

    He has actually done it

--- client/src/Components/homepage/WhoAreWe.js
+++ client/src/Components/homepage/WhoAreWe.js
@@ -1,11 +1,26 @@
 import React, {Component} from "react";
+import {Image} from "react-bootstrap";
+import Brian from "../../images/Brian.jpg";
 
 class WhoAreWe extends Component {
   //{*/ <button className="login" onClick={this.props.login}>Login with Spotify</button> */}
 
     render() {
-        return  <div className="WhoAreWe">
-                    <div className="TagLine">He's Done It.</div>
+        return  <div className="Backdrop">
+                  <div className="WhoAreWe">
+                    <div className="Creator">
+                      <div className="Info">
+                        <Image src={Brian} className="CreatorImg" alt="A photo of Brian."></Image>
+                        <div className="Name">Brian</div>
+                      </div>
+                      <div className="Description">
+                          Brian is a Computer Science student at UW-Madison. He plans to graduate this month!
+                          He has been a member of the Wisconsin Men's Club Water Polo team for the past 4 years as well.
+                          After graduation, Brian hopes to find a job in the Chicago area, where he is from. <br></br>
+                          "Thanks for checking out our project!"
+                        </div>
+                    </div>
+                  </div>
                 </div>
       }
 
